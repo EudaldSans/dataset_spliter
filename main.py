6ae7867a91e7117e3e830952051ec7528955658a
Eudald Sans <eudald.sans@gmail.com>
2023-06-12 15:21:04 +0200

    Implement algoirithm with OpenAI whisper

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import wave
+import math
+import sys
 import os
-import random
 from typing import List, Tuple
-from more_itertools import batched
+from pprint import pprint
 
 import numpy as np
-import webrtcvad
-import matplotlib.pyplot as plt
+import wave
+import whisper
+
+from tqdm import tqdm
+
 
 
 def load_wav(path: str) -> Tuple[np.ndarray, int]:
@@ -39,58 +42,34 @@ def load_text(path: str) -> List[str]:
     return line.split(' ')
 
 
-def find_gaps_in_audio(audio: np.ndarray, samplerate: int, vad: webrtcvad.Vad) -> List[Tuple[int, int]]:
-    audio_len = int(0.01 * samplerate)
-    samples = batched(audio, audio_len)
-    samples = [np.array(sample) for sample in samples]
-    minimum_speech = int(0.2 * samplerate)
+def main():
+    model = whisper.load_model('small')
+    wav_path = os.path.join('dataset', 'ey_lola_baja_todas_las_persianas.wav')
+    audio, sr = load_wav(wav_path)
+    result = model.transcribe(wav_path, verbose=True, word_timestamps=True)
+    pprint(result['segments'])
+    segments = result['segments']
 
-    sample_start = 0
-    detecting_voice = False
+    for segment in tqdm(segments, desc=f'Processing segment', file=sys.stdout):
+        words = segment['words']
 
-    voice_fragments = list()
-    for count, sample in enumerate(samples):
-        if not webrtcvad.valid_rate_and_frame_length(samplerate, len(sample)):
-            print(f'Padding sample with {audio_len - len(sample)} zeroes.')
-            sample = np.pad(sample, (0, audio_len - len(sample)), 'constant')
+        for word in words:
+            word_start = math.floor(word['start'] * sr)
+            word_end = math.ceil(word['end'] * sr)
+            '''word_duration = word_end - word_start
+            if word_duration < sr:
+                missing_samples = sr - word_duration
+                word_start = word_start - missing_samples // 2
+                word_end = word_end + missing_samples // 2
 
-        is_speech = vad.is_speech(sample.tobytes(), sample_rate=samplerate)
+                if word_start < 0: word_start = 0
+                if word_end > len(audio): word_end = len(audio)'''
 
-
-
-        if is_speech and not detecting_voice:
-            detecting_voice = True
-            sample_start = count * audio_len
-        elif not is_speech and detecting_voice:
-            sample_end = count * audio_len
-            # if sample_end - sample_start < minimum_speech: continue
-
-            detecting_voice = False
-            sample_positions = (sample_start, sample_end)
-            voice_fragments.append(sample_positions)
-
-
-
-    return voice_fragments
+            sample = audio[word_start: word_end]
+            detected_word = word['word'].strip(" ").strip(',')
+            save_path = os.path.join('results', f'{detected_word}.wav')
+            save_wav(save_path, sr, sample)
 
 
 if __name__ == '__main__':
-    audio, samplerate = load_wav(os.path.join('dataset', 'ey_lola_baja_todas_las_persianas.wav'))
-    words = load_text(os.path.join('dataset', 'ey_lola_baja_todas_las_persianas.txt'))
-    print(samplerate)
-    vad = webrtcvad.Vad(3)
-
-    voice_fragments = find_gaps_in_audio(audio, samplerate, vad)
-
-    if len(voice_fragments) != len(words): print(f'Found {len(voice_fragments)} but expected {len(words)} words')
-
-    if not os.path.exists('results'): os.mkdir('results')
-
-    for count, fragment in enumerate(voice_fragments):
-        start, end = fragment
-        audio_sample = audio[start: end]
-        save_wav(os.path.join('results', f'{words[count]}_{count}.wav'), samplerate, audio_sample)
-
-    print(audio)
-    print(voice_fragments)
-    print(len(voice_fragments))
+    main()
